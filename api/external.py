22d303103dccd5d1cacdf2b0d651b3f2753b084a
Adel Nehikhare <adelnehikhare@gmail.com>
2022-07-04 22:10:48 +0100

    I didnt

--- api/external.py
+++ api/external.py
@@ -1,16 +1,16 @@
 import requests
 
 class SMSService():
-
+    #Send SMS to recipient informing them of donation
     def donationComplete(phone, pharmacy):
         ENDPOINT = 'https://www.bulksmsnigeria.com/api/v1/sms/create'
         HEADERS = {
             'Content-Type' : 'application/json',
             'Accept' : 'application/json'
         }
-        #Bearer FLWSECK-6aad26f4c82fc347d8e1c98ed32fb5f4-X
+        
         DATA = {
-            "api_token": "p0E9gMfwDQBdgEPCLwe5aWQe0KadA3Imod50lWRN3YF8RCuIOwILZ9RpyER6",
+            "api_token": "ALL LIES",
             "to": phone,
             "from": "FreePad",
             "body": f'Your request has been fulfilled! You can pickup your pad at {pharmacy}',
@@ -25,9 +25,9 @@ class SMSService():
             'Content-Type' : 'application/json',
             'Accept' : 'application/json'
         }
-        #Bearer FLWSECK-6aad26f4c82fc347d8e1c98ed32fb5f4-X
+        
         DATA = {
-            "api_token": "p0E9gMfwDQBdgEPCLwe5aWQe0KadA3Imod50lWRN3YF8RCuIOwILZ9RpyER6",
+            "api_token": "ALL LIES",
             "to": phone,
             "from": "FreePad",
             "body": f'Thank you for your donation to {title}. We really appreciate',
